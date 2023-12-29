b492c51842e9313b97d54bb572fac2b1d4775170
PersonalButler <savaradovic@gmail.com>
2023-12-29 04:28:23 -0500

    Update Base.js

--- pages/Base.js
+++ pages/Base.js
@@ -1,21 +1,21 @@
 import React, {Component} from 'react';
 import Helmet from 'react-helmet';
 
-import Header from '../components/Header/Header';
-import Buttons from '../components/TestComponents/Buttons';
-import SampleText from '../components/TestComponents/SampleText';
-import PlayerCarousel from '../components/TestComponents/Carousel';
-import TeamStats from '../components/TestComponents/TeamStats';
-import Roster from '../components/TestComponents/Roster';
-import NBALogoPicture from '../components/TestComponents/NBALogoPicture'
-import HomePageHeader from '../components/TestComponents/HomePageHeader'
-import TeamsList from '../components/TestComponents/TeamsList'
-import NBATeamLogosPicture from '../components/TestComponents/NBATeamLogosPicture'
-import CurrentSeasonTeamStats from '../components/TestComponents/CurrentSeasonTeamStats'
+import Header from '/components/Header/Header';
+import Buttons from '/components/TestComponents/Buttons';
+import SampleText from '/components/TestComponents/SampleText';
+import PlayerCarousel from '/components/TestComponents/Carousel';
+import TeamStats from '/components/TestComponents/TeamStats';
+import Roster from '/components/TestComponents/Roster';
+import NBALogoPicture from '/components/TestComponents/NBALogoPicture'
+import HomePageHeader from '/components/TestComponents/HomePageHeader'
+import TeamsList from '/components/TestComponents/TeamsList'
+import NBATeamLogosPicture from '/components/TestComponents/NBATeamLogosPicture'
+import CurrentSeasonTeamStats from '/components/TestComponents/CurrentSeasonTeamStats'
 import  {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import { inject, observer } from 'mobx-react';
 
-class Base extends Component {
+class Index extends Component {
   render(){
     return (
       <div> 
